e7d18a510ba6fc746d9ea96ac6aeaa7280815147
Ildefonso Cases <icases@gmail.com>
2016-01-30 02:47:39 +0100

    remove an extra space

--- pdb/pdb.php
+++ pdb/pdb.php
@@ -7,7 +7,7 @@
 	//check token
 	if($token!=$config['token']){
 		http_response_code(401); 
-		exit("<h1>ERROR 401:Unauthorized</h1><p>  You are not authorized to use this service<p>\n");
+		exit("<h1>ERROR 401:Unauthorized</h1><p>You are not authorized to use this service<p>\n");
 		
 	}
 	//$reponse_url=$_GET['response_url']
